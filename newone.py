e9107e5aac61f53ac712ab3e64f1698772f4293e
beeleb <ontkys@gmail.com>
2014-05-26 18:28:39 +0900

    ・軸に矢印を入れるため、書き方を大幅に変更しました。 ・軸の文字をticksで入れるか、直接位置を指定して文字を入れるかで迷っています。(現状どちらにしてもいくつかの問題があります)……今回はとりあえず位置を直接指定しています

--- newone.py
+++ newone.py
@@ -1,24 +1,8 @@
 ﻿from __future__ import division #(p,q)区間をn-1等分するので小数が含まれる場合に対応させるため。
 from pylab import * 
 #pylabで数式をいろいろ定義してくれてるらしく、numpyいらなかったみたいです。
+from mpl_toolkits.axes_grid.axislines import SubplotZero
 import matplotlib.pyplot as plt
-def subplots():
-#このへんまだよくわからないです。目盛りは消えたけど軸に矢印がついてないし軸の名前の位置も違う。さらにb軸は向きも違う。
-    "Custom subplots with axes throught the origin"
-    fig, ax = plt.subplots()
-    # Set the axes through the origin
-    for spine in ['left', 'bottom']:
-        ax.spines[spine].set_position('zero')
-    for spine in ['right', 'top']:
-        ax.spines[spine].set_color('none')
-    ax.set(aspect=1) #アスペクト比を固定しました。
-    ax.set_xticks([]) 
-    ax.set_yticks([])
-    xlabel("a", fontname='serif') # x軸のタイトル
-    ylabel(r"b", fontname='serif') # y軸のタイトル
-    return fig, ax
-    
-fig, ax = subplots()
 
 #ここから下に変数が入っている。
 def f(x,a):
@@ -29,9 +13,36 @@ n = 12 #引く包絡線の数
 a_min = -10 #表示させるaの最小値
 a_max = 10 #表示させるaの最大値
 y_min = -6 #表示させるbの最小値(最大値はa軸とb軸の縮尺が1:1になるよう自動で決まる)……アスペクト比を定めただけだと以上に縦長なグラフが出てくるので。
+y_max = y_min + a_max - a_min #これは変数ではない
+
+fig = plt.figure(1)
+ax = SubplotZero(fig, 111)
+fig.add_subplot(ax)
+
+ax.axhline(linewidth=1.0, color="black")
+ax.axvline(linewidth=1.0, color="black")
+
+ax.set_xticks([])#ここに入れたx座標にticksが入る。
+ax.set_yticks([])#ここに入れたy座標にticksが入る。
+ax.set_xticklabels(['a'])#入れる文字
+ax.set_yticklabels(['b'])#入れる文字……y軸側は横に90度回転してしまう。難しいか……？
+#ここではちょっと下の直接入力をやっているので入れていない。最終的に直接入力を採用した場合はここは消す。
+ax.set(aspect=1)
+
+plt.figtext(0.85, 0.35, '$a$')#直接位置を指定している。この位置を変数使って表せればこれでいい……？
+plt.figtext(0.5, 0.95, '$b$')
+#ちょっと文字が小さいので大きくしたい。
+
+for direction in ["xzero", "yzero"]:
+    ax.axis[direction].set_axisline_style("-|>")
+    ax.axis[direction].set_visible(True)
+
+for direction in ["left", "right", "bottom", "top"]:
+    ax.axis[direction].set_visible(False)
+
+
 ylim(ymin = y_min) 
-ylim(ymax= y_min + a_max - a_min) 
-#変数ここまで。下3つは自動で定められると嬉しいけど……？
+ylim(ymax = y_max) 
 
 
 
@@ -41,7 +52,4 @@ for i in range(n):
 	r = p + (q - p) * i / (n - 1) #n個の接線を引き、2個は両端にあるので区間はn-1等分されるといいのかな、と考えました。
 	b = f(r, a)
         ax.plot(a, b, 'k', linewidth=0.5, alpha=1) #linewidthは線の太さ、alphaは濃さ(1以下)をそれぞれ表す。黒色の線は'k'で指定する。指定しないとカラフルでそれはそれで見やすい。
-show()	
-#定めた範囲内で包絡線すべてがおさまるように表示するbの範囲を(自動的に)決めているようなので、肝心の放物線(など)が小さく表示されすぎてしまう……。要改善。
-#bの値が一定以下になったところで切りたいけど、グラフによって「切りたい値」は変わってくる……？
-#プロットされたグラフを表示する際、a軸とb軸の比率は1:1になっているのか……？(なっていないような気がする)→aの最小、最大を用いてbの範囲も定められないか……？
+show()
